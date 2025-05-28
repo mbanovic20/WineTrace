e214676e040a03a34b73f66ff5d37343f011ef2c
mbanovic20 <mbanovicb@gmail.com>
2025-05-28 20:16:12 +0200

    WineTraceFactory created.

--- contracts/WineTrace.sol
+++ contracts/WineTrace.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract WineTracer {
+contract WineTrace {
     struct Stage {
         string description;
         string date;
---
+++ contracts/WineTraceFactory.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "./WineTrace.sol";
+
+contract WineTraceFactory {
+    address[] public wineContracts;
+
+    event WineTraceCreated(address indexed wineContract, address indexed owner);
+
+    function createWineTrace(
+        string memory _grapeType,
+        string memory _harvestYear,
+        uint _quantityKg
+    ) external {
+        WineTrace wine = new WineTrace(_grapeType, _harvestYear, _quantityKg, msg.sender);
+        wineContracts.push(address(wine));
+        emit WineTraceCreated(address(wine), msg.sender);
+    }
+}
